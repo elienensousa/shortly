12c9ed49234d7ffc3fcfe22cd7b8ffa0e45420b4
Eliene Sousa <eliene.nunes1@hotmail.com>
2022-07-12 01:31:15 -0300

    Componente Boost

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BenefitsSection } from './components/BenefitsSection';
+import { BoostSection } from './components/BoostSection';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import './styles/global.scss';
@@ -10,6 +11,7 @@ function App() {
       <Hero />
 
       <BenefitsSection />
+      <BoostSection />
     </>
   );
 }
---
+++ src/components/BoostSection/index.js
@@ -0,0 +1,12 @@
+import './styles.scss'
+
+export function BoostSection(){
+    return(
+        <>
+            <div className="boost_section">
+                <h3>Boost your links today</h3>
+                <button className="boost_button">Get Started</button>
+            </div>
+        </>
+    )
+}
